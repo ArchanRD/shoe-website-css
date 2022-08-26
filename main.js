a9269346fdb5028cc62a1e2a65f29498d7d6b31c
= <archanrd29@gmail.com>
2022-08-26 19:57:49 +0530

    bug fix hamburger menu

--- main.js
+++ main.js
@@ -3,20 +3,20 @@ let nav_items = document.querySelector('.nav-items');
 let nav_item_child = document.querySelectorAll('.nav-item-child');
 let closeBtn = document.getElementById('close');
 
-function toggle_menu(){
-    if(nav_items.classList.contains('show-nav')){
-        nav_items.classList.remove('show-nav');
-        menu.style.display = "block";
-        closeBtn.style.display = "none";
-    }else{
-        nav_items.classList.add('show-nav');
-        menu.classList.add('faded-animation');
-        menu.style.display = "none";
-        closeBtn.classList.add('faded-animation');
-        closeBtn.style.display = "block";
-    }
+nav_items.style.maxHeight = "0px";
 
-    
+function toggle_menu(){
+    if(nav_items.style.maxHeight == "0px"){
+        nav_items.style.maxHeight = "300px";
+    }else{
+        nav_items.style.maxHeight = "0px";
+    }
+}
+
+for(i=0; i<nav_item_child.length; i++){
+    nav_item_child[i].addEventListener('click', ()=>{
+        nav_items.style.maxHeight = "0px";
+    })
 }
 
 closeBtn.addEventListener('click', ()=>{
@@ -24,9 +24,3 @@ closeBtn.addEventListener('click', ()=>{
     menu.style.display = "block";
     closeBtn.style.display = "none";
 })
-
-for(i=0; i<nav_item_child.length; i++){
-    nav_item_child[i].addEventListener('click', function(){
-        nav_items.classList.remove('show-nav')
-    })
-}
